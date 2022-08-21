3a74ad5b8cd978010a0c3f184ae4dc49fe7b3031
Yuan Liu <yuanmiss6@gmail.com>
2022-08-21 23:09:05 +1000

    Initial commit for house power consume project.

---
+++ plot1.R
@@ -0,0 +1,39 @@
+#1. download the dataset.
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/data.zip", mode = "wb")
+
+setwd("~/Desktop/data") #make sure the zip file and working directory in the same folder
+unzip("data.zip")
+
+#2. read the datafile
+dataset <- read.table("~/Desktop/data/household_power_consumption.txt", sep = ";", header = TRUE)
+str(dataset)
+summary(dataset)
+
+#2.1 set the date to the date class
+dataset$Date <- dmy(dataset$Date)
+#2.2 select the data from the dates 2007-02-01 and 2007-02-02
+date_select <- with(dataset, dataset[(Date == "2007-02-01") | (Date == "2007-02-02") , ])
+table(date_select$Date) #check the selected date
+
+#2.3 change the columns class to numeric
+
+date_select[, 3:9] <- sapply(date_select[, 3:9], as.numeric)
+summary(date_select)
+
+#3. Construct the plots
+quartz(title = "plot1.png", width = 5, height = 5, bg = "white")#set the PNG file with a width of 
+#480 pixels and a height of 480 pixels
+
+with(date_select, hist(Global_active_power, col = "red", main = "Global Active Power", 
+                       xlab = "Global Active Power (kilowatts)",
+                       ylab = "Frequency"))
+quartz.save(file = "plot1.png", type = "png")
+
+
+
---
+++ plot4.R
@@ -0,0 +1,64 @@
+#1. download the dataset.
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./data/data.zip", mode = "wb")
+
+setwd("~/Desktop/data") #make sure the zip file and working directory in the same folder
+unzip("data.zip")
+
+#2. read the datafile
+dataset <- read.table("~/Desktop/data/household_power_consumption.txt", sep = ";", header = TRUE)
+str(dataset)
+summary(dataset) #overview the whole dataset
+
+#2.1 set the Date column to the date class & combine the Date and Time column to date_time
+dataset$date_time <- paste(dataset$Date, dataset$Time, sep = " ")
+dataset$date_time <- as.POSIXct(dataset$date_time, format = "%d/%m/%Y %H:%M:%S")
+dataset$Date <- dmy(dataset$Date)
+
+#2.2 select the data from the dates 2007-02-01 and 2007-02-02
+date_select <- with(dataset, dataset[(Date == "2007-02-01") | (Date == "2007-02-02") , ])
+table(date_select$Date) #check the selected date
+
+#2.3 change the columns class to numeric
+
+date_select[, 3:9] <- sapply(date_select[, 3:9], as.numeric)
+summary(date_select)
+
+#3. Construct the plots
+quartz(title = "plot1.png", width = 5, height = 5, bg = "white") #set the PNG file with a width of 
+#480 pixels and a height of 480 pixels
+par(mfrow = c(2, 2), cex = 0.5)
+
+#plot1
+with(date_select, plot(date_time, Global_active_power, type = "l", 
+                       xlab = "",
+                       ylab = "Global Active Power (kilowatts)"))
+
+#plot2
+with(date_select, plot(date_time, Voltage, type = "l", 
+                       xlab = "datetime",
+                       ylab = "Voltage"))
+
+#plot3
+attach(date_select)
+
+plot(date_time, Sub_metering_1, type = "l", 
+                       xlab = "",
+                       ylab = "Energy sub metering")
+lines(date_time, Sub_metering_2, type = "l", col = "red")
+lines(date_time, Sub_metering_3, type = "l", col = "blue")
+
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1, 1, 1), col = c("black", "red", "blue"))
+
+#plot4
+with(date_select, plot(date_time, Global_reactive_power, type = "l", 
+                       xlab = "datetime",
+                       ylab = "Global_reactive_power"))
+
+quartz.save(file = "plot4.png", type = "png")
